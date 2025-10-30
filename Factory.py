f0d73c8a756739a137636cb6232b566495b7cb92
WombelBat <stefan.bogdan.bodea@gmail.com>
2025-10-30 22:38:07 +0200

    set the nr of factories for current production

--- Factory.py
+++ Factory.py
@@ -1,9 +1,11 @@
-import factorio_obj as p
+from factorio_obj import ReadItemsFromFile,factorioItem 
 
+
+item_list: dict[str, factorioItem] = {}
 
 lost = "items.json"
 
-item_list = p.ReadItemsFromFile(lost)
+item_list = ReadItemsFromFile(lost)
 
 # objects with no propper structure:
 # 
@@ -15,24 +17,28 @@ copper_ore_output =0
 # Set number of factories for each item by editing the integers below.
 # Example: item_list["plastic"].setFactories(3)
 
-item_list["plastic"].setFactories(0)
-item_list["sulfur"].setFactories(0)
-item_list["Electronic circuit"].setFactories(0)
-item_list["iron plate"].setFactories(0)
-item_list["copper plate"].setFactories(0)
-item_list["Advanced circuit"].setFactories(0)
-item_list["Petroleum"].setFactories(0)
-item_list["steel plate"].setFactories(0)
-item_list["iron gear wheel"].setFactories(0)
-item_list["transport belt"].setFactories(0)
-item_list["copper cable"].setFactories(0)
+for i in item_list.values():
+    if i.getChosenMethod() =="":
+        i.chose_method(2)
+
+item_list["plastic"].setFactories(4)
+item_list["sulfur"].setFactories(4)
+item_list["Electronic circuit"].setFactories(5)
+item_list["iron plate"].setFactories(36)
+item_list["copper plate"].setFactories(18)
+item_list["Advanced circuit"].setFactories(8)
+item_list["Petroleum"].setFactories(14)
+item_list["steel plate"].setFactories(6)
+item_list["iron gear wheel"].setFactories(8)
+item_list["transport belt"].setFactories(6)
+item_list["copper cable"].setFactories(7)
 item_list["assembling machine 1"].setFactories(0)
-item_list["battery"].setFactories(0)
-item_list["sulfuric acid"].setFactories(0)
+item_list["battery"].setFactories(4)
+item_list["sulfuric acid"].setFactories(1)
 item_list["processing unit"].setFactories(0)
-item_list["engine unit"].setFactories(0)
+item_list["engine unit"].setFactories(8)
 item_list["electric engine unit"].setFactories(0)
-item_list["pipe"].setFactories(0)
+item_list["pipe"].setFactories(6)
 item_list["low density structure"].setFactories(0)
 item_list["rocket fuel"].setFactories(0)
 item_list["speed module"].setFactories(0)
@@ -40,21 +46,21 @@ item_list["efficiency module"].setFactories(0)
 item_list["productivity module"].setFactories(0)
 item_list["satellite"].setFactories(0)
 item_list["rocket part"].setFactories(0)
-item_list["automation science pack"].setFactories(0)
-item_list["logistic science pack"].setFactories(0)
-item_list["military science pack"].setFactories(0)
-item_list["chemical science pack"].setFactories(0)
+item_list["automation science pack"].setFactories(6)
+item_list["logistic science pack"].setFactories(14)
+item_list["military science pack"].setFactories(8)
+item_list["chemical science pack"].setFactories(16)
 item_list["production science pack"].setFactories(0)
 item_list["utility science pack"].setFactories(0)
 item_list["space science pack"].setFactories(0)
 item_list["speed module 2"].setFactories(0)
 item_list["productivity module 2"].setFactories(0)
 item_list["efficiency module 2"].setFactories(0)
-item_list["Stone brick"].setFactories(0)
-item_list["rail"].setFactories(0)
-item_list["wall"].setFactories(0)
-item_list["firearm magazine"].setFactories(0)
-item_list["piercing rounds magazine"].setFactories(0)
+item_list["Stone brick"].setFactories(6)
+item_list["rail"].setFactories(3)
+item_list["wall"].setFactories(6)
+item_list["firearm magazine"].setFactories(6)
+item_list["piercing rounds magazine"].setFactories(6)
 item_list["uranium rounds magazine"].setFactories(0)
 item_list["shotgun shells"].setFactories(0)
 item_list["piercing shotgun shells"].setFactories(0)
@@ -66,5 +72,17 @@ item_list["explosive rocket"].setFactories(0)
 item_list["atomic bomb"].setFactories(0)
 item_list["Flamethrower ammo"].setFactories(0)
 item_list["Explosives"].setFactories(0)
-item_list["grenade"].setFactories(0)
-item_list["inserter"].setFactories(0)
+item_list["grenade"].setFactories(6)
+item_list["inserter"].setFactories(4)
+
+item_list["inserter"].chose_method(1)
+item_list["transport belt"].chose_method(1)
+item_list["wall"].chose_method(1)
+item_list["automation science pack"].chose_method(1)
+item_list["logistic science pack"].chose_method(1)
+item_list["Electronic circuit"].chose_method(1)
+# just change the ones that have specific values
+
+
+item_list["processing unit"].factoriesTOgetOutputX(2,verbose = True)
+item_list["processing unit"].resourcesToGetOutputX(2,verbose=True)
